f905a1c1906e58be676c1920eeca8ea5e09ce34e
dev3Nigerian <faithfulodigbo@gmail.com>
2022-05-03 08:16:56 +0100

    added hardhat config

---
+++ hardhat/constants/index.js
@@ -0,0 +1,4 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x84999fd0b6976beca014e589f843793c5a4a9fc5";
+
+// URL to extract Metadata for a OdigboNFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
